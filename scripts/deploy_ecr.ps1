6e817ce448a64e2677ff0872eb82fa75ed76fc57
Jordan <jordangottardo@libero.it>
2022-08-26 15:22:12 +0200

    fix scripts

--- scripts/deploy_ecr.ps1
+++ scripts/deploy_ecr.ps1
@@ -1,3 +1,3 @@
-aws cloudformation deploy --template-file .\TgtgPoller_ecr_stack.json --stack-name TgTgPollerPrereq --capabilities CAPABILITY_NAMED_IAM  --profile Personal
+aws cloudformation deploy --template-file ..\infrastructure\TgtgPoller_ecr_stack.json --stack-name TgTgPollerEcrRepo --capabilities CAPABILITY_NAMED_IAM  --profile Personal
 
 Write-Output "ECR repository created successfully, ensure the right ECR URI is set in TgtgPoller_stack.json"
--- scripts/deploy_infra.ps1
+++ scripts/deploy_infra.ps1
@@ -1,3 +1,1 @@
-aws cloudformation deploy --template-file .\TgtgPoller_stack.json --stack-name TgTgPoller --capabilities CAPABILITY_NAMED_IAM --profile Personal
-
-Write-Output "Infrastructure deployed successfully"
+aws cloudformation deploy --template-file ..\infrastructure\TgtgPoller_stack.json --stack-name TgTgPoller --capabilities CAPABILITY_NAMED_IAM --profile Personal
