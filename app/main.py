acfbae80ede5f661db1d94704f232cd96194722f
Jordan <jordangottardo@libero.it>
2022-09-09 10:51:24 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ from mangum import Mangum
 import boto3
 from tokens.dynamo_db_tokens_client import DynamoDbTokensClient
 from tokens.tokens_repository import TokensRepository
-from products.dynamo_db_products_client import DynamoDbProductsClient
-from products.products_repository import ProductsRepository
+from dynamo_db_products_client import DynamoDbProductsClient
+from products_repository import ProductsRepository
 
 from tgtg.too_good_to_go_client import TooGoodToGoClient
 
