0d9bcb7c50d287fe5fabb01ddca0cee378a29037
Jordan <jordangottardo@libero.it>
2022-09-30 16:13:16 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -55,13 +55,9 @@ def get_available_products(userEmail: str):
 
 @app.post("/products/update")
 def update_products(userEmail: str):
-    logger.info(f"Proxies= {proxies}")
-    logger.info(f"EnvVars= {os.environ}")
-
-
     tokens = tokensRepository.get_tokens(userEmail)
     tgtgClient = TooGoodToGoClient(
-        None, tokens.accessToken, tokens.refreshToken, tokens.userId, proxies)
+        None,proxies, tokens.accessToken, tokens.refreshToken, tokens.userId)
     products = tgtgClient.get_items()
 
     logger.info(f"Products from TgTg: {products}")
