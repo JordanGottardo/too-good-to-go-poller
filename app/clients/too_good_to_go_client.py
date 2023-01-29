8a0f546cbcebfb7c938211d85edb5a6f2a41d9ad
JordanG <jordangottardo@gmail.com>
2023-01-29 13:07:57 +0100

    add cookie

--- app/clients/too_good_to_go_client.py
+++ app/clients/too_good_to_go_client.py
@@ -4,14 +4,14 @@ from tgtg import TgtgClient
 
 class TooGoodToGoClient:
 
-    def __init__(self, email, proxies, accessToken=None, refreshToken=None, userId=None):
+    def __init__(self, email, proxies, accessToken=None, refreshToken=None, userId=None, cookie=None):
         self.__initLogging()
 
         if (accessToken is not None):
             self.logger.info(
                 f"TooGoodToGoClient Constructor: initializing for user with id {userId}, access token {accessToken}, refresh token {refreshToken}")
             self.client = TgtgClient(
-                access_token=accessToken, refresh_token=refreshToken, user_id=userId, proxies=proxies)
+                access_token=accessToken, refresh_token=refreshToken, user_id=userId, cookie=cookie, proxies=proxies)
         else:
             self.logger.info(
                 f"TooGoodToGoClient Constructor: initializing for user with email {email}")
